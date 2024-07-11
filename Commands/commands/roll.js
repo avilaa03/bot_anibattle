e67607a1db4e91757f8943d9c515b6236f325a75
avilaa03 <lucasdeavila2003@gmail.com>
2024-07-11 00:17:04 -0300

    Roll agora armazena cartas no inventário no banco de dados por usuário quando clica no botão, não deixa pessoas fora da interação clicar no botão e faz verificação de tempo para coleta da carta.

--- Commands/commands/roll.js
+++ Commands/commands/roll.js
@@ -2,13 +2,14 @@ const BaseSlashCommand = require('../utils/BaseSlashCommand');
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const Card = require('../utils/cardSchema.js');
 const User = require('../utils/userSchema.js');
+const mongoose = require('mongoose');
 
 module.exports = class RollSlashCommand extends BaseSlashCommand {
     constructor() {
         super('roll');
     }
 
-    run(client, interaction) {
+    async run(client, interaction) {
         User.findOne({ id: interaction.user.id }, (err, user) => {
             if (err) {
                 console.error('Erro ao buscar as informações do usuário:', err);
@@ -18,12 +19,21 @@ module.exports = class RollSlashCommand extends BaseSlashCommand {
             const now = Date.now();
 
             if (user && user.lastRoll && now - user.lastRoll < 1 * 60 * 1000) {
-                return interaction.reply('Você só pode rolar uma vez a cada 1 minuto.');
+                const timeElapsed = now - user.lastRoll;
+                const timeRemaining = 1 * 60 * 1000 - timeElapsed;
+            
+                const seconds = Math.floor((timeRemaining % (60 * 1000)) / 1000);
+                return interaction.reply(`Você só pode rolar uma vez a cada 1 minuto (TEMPO DE TESTES). Faltam ${seconds} segundos para você roletar novamente.`);
             }
 
-            if (user && user.lastRoll && now - user.lastRoll < 15 * 60 * 1000) {
-                return interaction.reply('Você só pode rolar uma vez a cada 15 minutos.');
-            }
+            // if (user && user.lastRoll && now - user.lastRoll < 15 * 60 * 1000) {
+            //     const timeElapsed = now - user.lastRoll;
+            //     const timeRemaining = 15 * 60 * 1000 - timeElapsed;
+            
+            //     const minutes = Math.floor(timeRemaining / (60 * 1000));
+            //     const seconds = Math.floor((timeRemaining % (60 * 1000)) / 1000);
+            //     return interaction.reply(`Você só pode rolar uma vez a cada 15 minutos. Faltam ${minutes} minutos e ${seconds} segundos para você roletar novamente.`);
+            // }
 
             const rarities = [
                 { rarity: 'Common', percentage: 70 },
@@ -61,48 +71,70 @@ module.exports = class RollSlashCommand extends BaseSlashCommand {
 
                     if (!card) {
                         console.warn('Nenhuma carta encontrada com a raridade especificada:', rarity);
-                        // Fallback para buscar uma carta aleatória se nenhuma carta for encontrada com a raridade especificada
-                        Card.findOne().exec((err, fallbackCard) => {
-                            if (err || !fallbackCard) {
-                                console.error('Erro ao buscar a carta de fallback:', err);
-                                return interaction.reply('Houve um erro ao buscar as informações da carta.');
-                            }
-
-                            card = fallbackCard;
-                            sendCardEmbed(card);
-                        });
-                    } else {
-                        sendCardEmbed(card);
+                        return interaction.reply('Nenhuma carta encontrada com a raridade especificada.');
                     }
+
+                    const row = new ActionRowBuilder()
+                        .addComponents(
+                            new ButtonBuilder()
+                                .setCustomId(`enviarInventario_${card._id}_${interaction.user.id}`)
+                                .setLabel('Enviar ao Inventário')
+                                .setStyle(ButtonStyle.Primary)
+                        );
+
+                    const embed = new EmbedBuilder()
+                        .setTitle('Carta Sorteada')
+                        .addFields(
+                            { name: "Nome", value: card.name.charAt(0).toUpperCase() + card.name.slice(1) },
+                            { name: "Raridade", value: card.rarity }
+                        )
+                        .setImage(card.image);
+
+                    interaction.reply({ embeds: [embed], components: [row] });
+
+                    if (!user) {
+                        user = new User({ id: interaction.user.id });
+                    }
+
+                    user.lastRoll = now;
+                    user.save();
+
+                    const filter = (i) => i.customId.startsWith(`enviarInventario_${card._id}`) && i.user.id === interaction.user.id;
+                    const collector = interaction.channel.createMessageComponentCollector({ filter, time: 30000 });
+
+                    collector.on('collect', async (i) => {
+
+                        const clonedCard = {
+                            cardId: new mongoose.Types.ObjectId(),
+                            originalCardId: card._id,
+                            number: card.number,
+                            name: card.name,
+                            series: card.series,
+                            image: card.image,
+                            rarity: card.rarity,
+                            ovr: card.ovr,
+                            ata: card.ata,
+                            int: card.int,
+                            def: card.def,
+                            des: card.des,
+                            pow: card.pow,
+                            res: card.res,
+                            obtainedAt: new Date()
+                        };
+
+                        user.inventory.push(clonedCard);
+                        await user.save();
+                        await i.update({ content: 'Carta enviada ao seu inventário!', components: [] });
+                        collector.stop('collected');
+                    });
+
+                    collector.on('end', (collected, reason) => {
+                        if (reason === 'time') {
+                            interaction.followUp({ content: 'O tempo para coletar a carta expirou.', components: [] });
+                        }
+                    });
                 });
             });
-
-            const sendCardEmbed = (card) => {
-                const row = new ActionRowBuilder()
-                    .addComponents(
-                        new ButtonBuilder()
-                            .setCustomId('enviarInventario')
-                            .setLabel('Enviar ao Inventário')
-                            .setStyle(ButtonStyle.Primary)
-                    );
-
-                const embed = new EmbedBuilder()
-                    .setTitle('Carta Sorteada')
-                    .addFields(
-                        { name: "Nome", value: card.name.charAt(0).toUpperCase() + card.name.slice(1) },
-                        { name: "Raridade", value: card.rarity }
-                    )
-                    .setImage(card.image);
-
-                interaction.reply({ embeds: [embed], components: [row] });
-
-                if (!user) {
-                    user = new User({ id: interaction.user.id });
-                }
-
-                user.lastRoll = now;
-                user.save();
-            };
         });
     }
 
--- Commands/utils/userSchema.js
+++ Commands/utils/userSchema.js
@@ -10,7 +10,24 @@ const UserSchema = new Schema({
     lastRoll: {
         type: Number,
         default: 0
-    }
+    },
+    inventory: [{
+        cardId: mongoose.Schema.Types.ObjectId,
+        originalCardId: mongoose.Schema.Types.ObjectId,
+        number: Number,
+        name: String,
+        series: String,
+        image: String,
+        rarity: String,
+        ovr: Number,
+        ata: Number,
+        int: Number,
+        def: Number,
+        des: Number,
+        pow: Number,
+        res: Number,
+        obtainedAt: { type: Date, default: Date.now } 
+    }]
 });
 
 const User = mongoose.model('User', UserSchema);
