0c4b5876245d1a4daed62d5300404117042ef8bb
avilaa03 <lucasdeavila2003@gmail.com>
2024-07-13 02:54:45 -0300

    Show mudado para somente o inventário do jogador, não será mais possível ter acesso a todas as cartas do bot pois elas não serão únicas.

--- Commands/commands/show.js
+++ Commands/commands/show.js
@@ -1,48 +1,98 @@
 const BaseSlashCommand = require('../utils/BaseSlashCommand');
-const { SlashCommandBuilder } = require('discord.js');
-const Card = require('../utils/cardSchema.js');
-const { EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const User = require('../utils/userSchema.js');
+
 module.exports = class ShowSlashCommand extends BaseSlashCommand {
     constructor() {
         super('show');
     }
 
-    run(client, interaction) {
-        const name = interaction.options.getString('character').toLowerCase();
-        Card.find({ name }, (err, cards) => {
-            if (cards.length === 0 || err) {
-                return interaction.reply('Personagem não encontrado');
-            }
-            else {
-                const embed = new EmbedBuilder()
+    async run(client, interaction) {
+        const name = interaction.options.getString('name').toLowerCase();
+
+        const user = await User.findOne({ id: interaction.user.id });
+
+        if (!user || user.inventory.length === 0) {
+            return interaction.reply('Seu inventário está vazio ou o usuário não foi encontrado.');
+        }
+
+        const matchingCards = user.inventory.filter(c => c.name.toLowerCase().includes(name));
+
+        if (matchingCards.length === 0) {
+            return interaction.reply('Nenhuma carta encontrada com esse nome.');
+        }
+
+        let currentIndex = 0;
+        const updateEmbed = () => {
+            const card = matchingCards[currentIndex];
+            const embed = new EmbedBuilder()
                 .setTitle('Anime Fight')
-            for(const card of cards) {
-                let words = card.name.split(" ");
-                if (words.length >= 2) {
-                    words.splice(1);
-                  }
-                card.name = words.join(" ");
-                embed.addFields(
-                {name: "Nome", value: card.name.charAt(0).toUpperCase() + card.name.slice(1)},
-                {name: "Série", value: card.series},
-                {name: "Raridade", value: card.rarity}
-                )
-                .setImage(card.image);
+                .setImage(card.image)
+                .addFields(
+                    { name: "Nome", value: card.name.charAt(0).toUpperCase() + card.name.slice(1) },
+                    { name: "Série", value: card.series },
+                    { name: "Raridade", value: card.rarity }
+                );
+
+            return embed;
+        };
+
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('prev')
+                    .setLabel('Anterior')
+                    .setStyle(ButtonStyle.Primary)
+                    .setDisabled(matchingCards.length === 1),
+                new ButtonBuilder()
+                    .setCustomId('next')
+                    .setLabel('Próximo')
+                    .setStyle(ButtonStyle.Primary)
+                    .setDisabled(matchingCards.length === 1)
+            );
+
+        const message = await interaction.reply({ embeds: [updateEmbed()], components: [row], fetchReply: true });
+
+        const filter = i => i.user.id === interaction.user.id;
+        const collector = message.createMessageComponentCollector({ filter, time: 60000 });
+
+        collector.on('collect', async i => {
+            if (i.customId === 'prev') {
+                currentIndex = (currentIndex - 1 + matchingCards.length) % matchingCards.length;
+            } else if (i.customId === 'next') {
+                currentIndex = (currentIndex + 1) % matchingCards.length;
             }
 
-            return interaction.reply({embeds: [embed]});
-        }
+            await i.update({ embeds: [updateEmbed()], components: [row] });
+        });
+
+        collector.on('end', collected => {
+            const disabledRow = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId('prev')
+                        .setLabel('Anterior')
+                        .setStyle(ButtonStyle.Primary)
+                        .setDisabled(true),
+                    new ButtonBuilder()
+                        .setCustomId('next')
+                        .setLabel('Próximo')
+                        .setStyle(ButtonStyle.Primary)
+                        .setDisabled(true)
+                );
+
+            message.edit({ components: [disabledRow] });
         });
     }
 
     getSlashCommandJSON() {
         return new SlashCommandBuilder()
-        .setName(this.name)
-        .setDescription('show card command')
-        .addStringOption(option =>
-            option
-            .setName('character')
-            .setDescription('Personagem que está procurando')
-            .setRequired(true));
+            .setName(this.name)
+            .setDescription('Mostra uma carta do seu inventário pelo nome')
+            .addStringOption(option =>
+                option.setName('name')
+                    .setDescription('O nome da carta que você quer mostrar')
+                    .setRequired(true)
+            );
     }
-}
+};
