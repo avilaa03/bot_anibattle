66480c985ae8d3e71c09dff05280c751ead3e672
avilaa03 <lucasdeavila2003@gmail.com>
2024-07-13 02:45:43 -0300

    Adicionado novos comandos e mudanças de comportamentos de botões existentes, removido /showall

--- Commands/commands/help.js
+++ Commands/commands/help.js
@@ -11,16 +11,16 @@ module.exports = class HelpSlashCommand extends BaseSlashCommand {
         
         const embed = new EmbedBuilder()
           .setTitle('Comandos')
-          .addFields({name: "/roll", value: "Comando para roletar um personagem aleatório"},
-            {name: "/show", value: "Comando para mostrar um personagem, colocando o nome dele depois do /show"},
-            {name: "/showall", value: "Mostra todos os nomes de todos os personagens listados no Bot"},
+          .addFields({name: "/roll", value: "Comando para roletar um personagem aleatório, podendo coletar para o inventário ou vender rapidamente"},
+            {name: "/show", value: "Comando para mostrar uma carta do seu inventário, utilize o nome da carta para procurá-la"},
             {name: "/inventory", value: "Mostra o inventário com todas as cartas armazenadas"},
+            {name: "/quicksell", value: "Venda rápida de uma carta, utilize o nome da carta para vendê-la"},
             {name: "/balance", value: "Utilizado para ver quanto tem no seu cofre, ou no de outro usuário, marcando-o depois de /balance"},
             {name: "/daily", value: "Resgatar recompensa diária"},
             {name: "/give", value: "Comando para dar dinheiro para outro usuário, marcando-o e escrevendo a quantia."},
             {name: "/info", value: "Informações do Bot"},
             {name: "/ping", value: "Não é muito útil, mas você pode tentar"},
-            {name: "/dice", value: "Literalmente um dado, role de 1 a 6"},
+            {name: "/dice", value: "Literalmente um dado, role de 1 a 6"}
             )
 
         interaction.reply({ embeds: [embed] });
