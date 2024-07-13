3d14f40a15dfd719d88ea7d94dd4d01d0664f21e
avilaa03 <lucasdeavila2003@gmail.com>
2024-07-13 02:56:09 -0300

    Comando obsoleto.

---
+++ OldCommands/showall.js
@@ -0,0 +1,32 @@
+// const BaseSlashCommand = require('../Commands/utils/BaseSlashCommand.js');
+// const { SlashCommandBuilder } = require('discord.js');
+// const Card = require('../Commands/utils/cardSchema.js');
+
+// module.exports = class ShowSlashCommand extends BaseSlashCommand {
+//     constructor() {
+//         super('showall');
+//     }
+
+//     run(client, interaction) {
+//         Card.find({ }, (err, cards) => {
+//             if (cards.length === 0 || err) {
+//                 return interaction.reply('Personagem não encontrado');
+//             }
+//             else {
+//             let reply = '';
+//             for(const card of cards) {
+//                 reply += `${card.number}: ${card.name.charAt(0).toUpperCase() + card.name.slice(1)}\n`;
+//             }
+
+//             return interaction.reply(reply);
+//         }
+//         });
+//     }
+
+//     getSlashCommandJSON() {
+//         return new SlashCommandBuilder()
+//         .setName(this.name)
+//         .setDescription('show all cards command')
+//         .toJSON();
+//     }
+// }
